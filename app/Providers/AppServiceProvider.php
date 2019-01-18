4e5e48015d195dde8985995ea737b829550d41d6
Amine EL G <aelghayate@simplon.co>
2019-01-19 00:27:29 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
         \Blade::if('admin', function() {
             return !auth()->guest() && auth()->user()->isAdmin();
         });
+        
+        Schema::defaultStringLength(191);
     }
 
     /**
